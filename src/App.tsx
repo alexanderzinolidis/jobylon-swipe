11d1bcdb0d85e587b5aae1a83d831188e23df4d4
alexanderzinolidis <a.zinolidis@gmail.com>
2023-02-23 14:32:45 +0100

    add wrapper class & documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Switch, Route } from "wouter"
-
 import JobListPage from "./pages/JobListPage"
 import NotFoundPage from "./pages/NotFoundPage"
 
-function App() {
+/*
+    Nothing fancy here, just using wouter to route `/` to the job list page.
+    If no route matches, a 404 page is rendered.
+
+    Obviously, wouter is overkill for this simple demo, but in the real world
+    we'd use it to route to other pages, such as a job details page when clicking
+    on a job card.
+*/
+
+export default function App() {
     return (
-        <div className="App">
+        <div className="App bg-[#4992fc] h-screen flex items-center justify-center overflow-y-hidden">
             <Switch>
                 <Route path="/" component={JobListPage} />
                 <Route component={NotFoundPage} />
@@ -13,5 +21,3 @@ function App() {
         </div>
     )
 }
-
-export default App
